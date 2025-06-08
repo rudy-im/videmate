610777145be67ba1b945eb4c9ac667b0d395e917
Rudy Im <rudy.im@outlook.com>
2025-06-07 21:33:24 -0400

    blender head bone

--- blender bpy/blender_rig_motion.py
+++ blender bpy/blender_rig_motion.py
@@ -5,8 +5,8 @@ rig = bpy.data.objects['metarig']
 bpy.context.view_layer.objects.active = rig
 bpy.ops.object.mode_set(mode='POSE')
 
-bone = rig.pose.bones['lip.T.R']
+bone = rig.pose.bones['spine.006']
 bone.rotation_mode = 'XYZ'
-bone.rotation_euler = (0.0, 0.0, math.radians(60))
+bone.rotation_euler = (0.0, math.radians(60), 0.0)
 
-bone.keyframe_insert(data_path="rotation_euler", frame=1)
+#bone.keyframe_insert(data_path="rotation_euler", frame=1)

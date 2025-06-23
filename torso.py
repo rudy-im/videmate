8fe60ef995d94d6b51d29cb1cacf5928e7ae3b2a
Rudy Im <rudy.im@outlook.com>
2025-06-22 20:38:38 -0400

    torso dict

---
+++ torso.py
@@ -0,0 +1,29 @@
+
+torso = {
+    'rCheek': 8,
+    'rEyeOuter': 6,
+    'rEyeCenter': 5,
+    'rEyeInner': 4,
+    'Nose': 0,
+    'lEyeInner': 1,
+    'lEyeCenter': 2,
+    'lEyeOuter': 3,
+    'lCheek': 7,
+
+    'rMouth': 10,
+    'lMouth': 9,
+
+    'rShoulder': 12,
+    'rElbow': 14,
+    'rWrist': 16,
+    'rThumb': 22,
+    'rPalmInner': 20,
+    'rPalmOuter': 18,
+
+    'lShoulder': 11,
+    'lElbow': 13,
+    'lWrist': 15,
+    'lThumb': 21,
+    'lPalmInner': 19,
+    'lPalmOuter': 17
+}
